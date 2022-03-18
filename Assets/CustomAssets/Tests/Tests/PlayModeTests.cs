2dad4f2b640ba45ea4f650464587406a41f2ddbd
Vahv <vahvett@gmail.com>
2022-03-18 15:28:06 +0200

    Finished TestSet3 which has been improved according to UnityMutator results. All non-equivalent mutants should be killed now.

--- Assets/CustomAssets/Tests/Tests/PlayModeTests.cs
+++ Assets/CustomAssets/Tests/Tests/PlayModeTests.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
@@ -16,6 +19,9 @@ public class PlayModeTests
     Transform player;
     Vector3 origPos;
     string mainSceneName = "SampleScene";
+
+    // How Approx-Asserts can deviate without it resulting to FAIL
+    float differenceTolerance = 0.001f;
 
     [OneTimeSetUp]
     public void OneTimeSetup()
@@ -135,12 +141,14 @@ public class PlayModeTests
     [UnityTest]
     public IEnumerator InterruptJump()
     {
+        float timeout = 3f; // Stop test if timeout has passed
+
         // Releasing space-key after interruptTime should result in player never getting higher than shortJumpHeight
         float interruptTime = 0.15f;
         float shortJumpHeight = 1f;
-        // AT LEAST how long it takes for player to start dropping after releasing jump-key
-        float dropStartDuration = 0.05f;
-        Debug.Log($"origPos.y + maxJumpHeight: {origPos.y + shortJumpHeight}");
+
+        float maxHeight = origPos.y + shortJumpHeight; // Player should never get above this
+        Debug.Log($"maxHeight (origPos + shortJumpHeight): {origPos.y + shortJumpHeight}");
 
         // Jump
         input.Press(keyboard.spaceKey);
@@ -148,33 +156,17 @@ public class PlayModeTests
         // Release after interrupt time
         input.Release(keyboard.spaceKey);
 
-        // Save highest position and compare to original position, should be less than shortJumpHeight
-        Vector2 releasePos = player.position;
-        Debug.Log($"Y-position at release was: {releasePos.y}");
-        Assert.Less(releasePos.y, origPos.y + shortJumpHeight);
-
-        // Assert that jump 'overflows' for a little while after releasing jump-key
-        yield return new WaitForSeconds(dropStartDuration);
-        Debug.Log($"Y-position after dropStartDuration: {player.position.y}");
-        Assert.Greater(player.position.y, releasePos.y);
-
-        // Wait until player starts dropping down
-        while (player.position.y >= releasePos.y)
-        {
-            yield return null;
-        }
-        Debug.Log("Player started dropping down");
-
-        // Init lastFramePos as high number at start
-        Vector2 lastFramePos = Vector2.up * 10;
-        // Check that position gets lower in each frame after dropping down, until player is on the ground again
+        // Check until player is back on original ground position
         while (!Mathf.Approximately(player.position.y, origPos.y))
         {
-            Assert.Less(player.position.y, lastFramePos.y);
-            lastFramePos = player.position;
             yield return null;
+
+            // Stop loop if timeout has passed
+            timeout -= Time.deltaTime;
+            if (timeout < 0) Assert.Fail();
+
+            Assert.Less(player.position.y, maxHeight);
         }
-        Debug.Log("Player landed");
     }
 
     // Verifies that player can't jump again until they have landed from previous jump
@@ -406,6 +398,294 @@ public class PlayModeTests
         AssertVectorsApproxEqual(endPos, player.transform.position);
     }
 
+    // ========== Test Set 3 Improvements ========== 
+
+    // Also improved InteeruptJump-test to kill mutations 32, 33
+
+    // Verifies that enemy sprite is flipped when it changes facing direction
+    // Should kill mutation 4 and mutation 5
+    [UnityTest]
+    public IEnumerator FlipEnemySprite()
+    {
+        // TODO figure out how to timeout this if WaitUntils are never satisfied
+        float timeoutLimit = 10f;
+        // Test not started until enemy is this far away from PatrolPath start/end
+        float startPointThreshold = 0.5f; 
+        // Move player to position where it's close to an enemy
+        Vector3 newStartPos = new Vector3(19, 1, 1);
+        player.position = newStartPos;
+
+        // Get the needed enemy components
+        GameObject enemy = GetClosestEnemy(player.position);
+        SpriteRenderer enemySr = enemy.GetComponent<SpriteRenderer>();
+        GameObject pp = GetClosestPatrolPath(player.position);
+
+        // Get PatrolPath start and end positions as scene coordinates
+        float ppStartX = pp.GetComponent<PatrolPath>().startPosition.x + pp.transform.position.x;
+        float ppEndX = pp.GetComponent<PatrolPath>().endPosition.x + pp.transform.position.x;
+
+        // Wait until enemy is not at the end points of patrol path to avoid weird test fails
+        yield return new WaitUntil(() => enemy.transform.position.x - ppEndX < -startPointThreshold
+        && enemy.transform.position.x - ppStartX > startPointThreshold);
+
+        // Save enemy startPosX
+        float startPosX = enemy.transform.position.x;
+
+        // Wait until moving left
+        yield return new WaitUntil(() => enemy.transform.position.x < startPosX);
+        Debug.Log("Enemy moving left");
+        // Assert that sprite is flipped
+        Assert.IsTrue(enemySr.flipX);
+
+        // Wait until moving right
+        yield return new WaitUntil(() => enemy.transform.position.x > startPosX);
+        Debug.Log("Enemy moving right");
+        // Assert that sprite is not flipped
+        Assert.IsTrue(!enemySr.flipX);
+    }
+
+    // Verifies that a patrolling enemy doesn't jump
+    // Should kill mutation 6 and mutation 7
+    [UnityTest]
+    public IEnumerator PatrollingEnemyNoJump()
+    {
+        float monitorForJump = 5f; // How long test should run and keep checking that enemy isn't jumping
+        float tolerance = 0.01f; // How much difference is tolerated in enemy position or height
+
+        // Move player to position where it's close to a patrolling enemy
+        Vector3 newStartPos = new Vector3(19, 1, 1);
+        player.position = newStartPos;
+
+        // Get the needed enemy components
+        GameObject enemy = GetClosestEnemy(player.position);
+        CapsuleCollider2D enemyCollider = enemy.GetComponent<CapsuleCollider2D>();
+
+        // Get enemy values which show when enemy jumps
+        float enemyStartY = enemy.transform.position.y;
+        float enemyStartColliderHeight = enemyCollider.size.y;
+
+        while(monitorForJump > 0)
+        {
+            // Assert that enemy.y and collider.y don't grow --> enemy hasn't jumped
+            Assert.Less(enemyCollider.size.y, enemyStartColliderHeight + tolerance);
+            Assert.Less(enemy.transform.position.y, enemyStartY + tolerance);
+
+            // Wait for next frame
+            monitorForJump -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    // Verifies player x-velocity is reset when hitting something
+    [UnityTest]
+    public IEnumerator PlayerStopRunWhenColliding()
+    {
+        float moveTimeUntilWall = 2.5f;
+        float monitorSpeedDuration = 2f;
+
+        PlayerController pc = player.GetComponent<PlayerController>();
+
+        // Move player to run against wall
+        input.Press(keyboard.aKey);
+        yield return new WaitForSeconds(moveTimeUntilWall);
+
+        // Monitor for a duration
+        while (monitorSpeedDuration > 0)
+        {
+            // Assert that player x-speed stays at 0 while trying to run against a wall
+            Assert.AreEqual(0, pc.velocity.x);
+
+            // Wait for next frame
+            monitorSpeedDuration -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    // Verifies that player y-velocity is reset when hitting something in the air
+    // Should kill mutation 17
+    [UnityTest]
+    public IEnumerator PlayerStopJumpWhenColliding()
+    {
+        float maxHeightUnderTile = -0.27f; // Player can't be above this under the tile
+        float monitorHeightDuration = 3f; // How long test should run and keep checking that player isn't above tile
+
+        // Move player to position which is under a tile
+        Vector3 underTilePos = new Vector3(19, -0.575f, 1);
+        player.position = underTilePos;
+
+        DisableEnemies();
+
+        // Press jump and movement so player jumps and moves forward from under the tile while in air
+        input.Press(keyboard.spaceKey);
+        input.Press(keyboard.dKey);
+
+        // Assert that player doesn't go above tile at any point because hitting the tile will reset player y-speed
+        while (monitorHeightDuration > 0)
+        {
+            Assert.Less(player.transform.position.y, maxHeightUnderTile);
+
+            // Wait for next frame
+            monitorHeightDuration -= Time.deltaTime;
+            yield return null;
+        }
+
+        input.Release(keyboard.spaceKey);
+        input.Release(keyboard.dKey);
+        yield return null;
+    }
+
+    // Verifies that player bounces up a little after killing enemy by jumping on it
+    // Should kill mutation 18
+    [UnityTest]
+    public IEnumerator PlayerBounceAfterEnemyKill()
+    {
+        float bounceHeight = 0.1f;
+        float bounceDuration = 0.2f;
+        float jumpDuration = 1f;
+
+        // Get first enemy found in the scene and reset its movement so it will stay still under player
+        GameObject enemy = GameObject.Find("Enemy");
+        CapsuleCollider2D enemyCollider = enemy.GetComponent<CapsuleCollider2D>();
+        enemy.GetComponent<EnemyController>().path = null;
+        enemy.GetComponent<AnimationController>().move = Vector2.zero;
+        float enemyStartY = enemy.transform.position.y;
+
+        // Start Jump
+        input.Press(keyboard.spaceKey);
+        // Wait until jump is half way done and move enemy to player's original position
+        yield return new WaitForSeconds(jumpDuration / 2);
+        enemy.transform.position = origPos;
+        // Release Jump-key after whole jumpDuration has passed to drop on top of enemy
+        yield return new WaitForSeconds(jumpDuration / 2);
+        input.Release(keyboard.spaceKey);
+
+        // Wait until enemy has died and save player position at moment of its death
+        yield return new WaitUntil(() => enemyCollider.enabled == false);
+        Vector3 playerPosAtKill = player.transform.position;
+        Debug.Log($"PlayerPos at kill: {playerPosAtKill.y}");
+
+        // Wait for bounce and assert that player position is at bounce height after it
+        yield return new WaitForSeconds(bounceDuration);
+        Debug.Log($"PlayerPos at bounce duration: {player.transform.position.y}");
+        Assert.Greater(player.transform.position.y, playerPosAtKill.y + bounceHeight);
+    }
+
+    // Verifies that different tabs can be opened in Pause-menu
+    // Should kill mutation 22
+    [UnityTest]
+    public IEnumerator OpenMenuTabs()
+    {
+        // Open menu and get UI-buttons
+        input.PressAndRelease(keyboard.escapeKey);
+        Button settingsButton = GameObject.Find("SettingsButton").GetComponent<Button>();
+        Button controlsButton = GameObject.Find("ControlsButton").GetComponent<Button>();
+        Button creditsButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+
+        // Click through all buttons and check that correct tab is open after it
+        // Clicks should be simulated through Mouse-input but that doesn't seem to work
+        settingsButton.onClick.Invoke();
+        GameObject settingsScreen = GameObject.Find("Settings");
+        Assert.IsTrue(settingsScreen.activeInHierarchy);
+
+        controlsButton.onClick.Invoke();
+        GameObject controlsScreen = GameObject.Find("Controls");
+        Assert.IsTrue(controlsScreen.activeInHierarchy);
+
+        creditsButton.onClick.Invoke();
+        GameObject creditsScreen = GameObject.Find("Credits");
+        Assert.IsTrue(creditsScreen.activeInHierarchy);
+
+        yield return null;
+    }
+
+    // Verifies that patrolling enemy moves at correctish speed
+    // Should kill mutation 28
+    [UnityTest]
+    public IEnumerator EnemyPatrolSpeed()
+    {
+        float travelTime = 1.75f; // How long it should take for enemy to travel this patrol path
+        float tolerance = 0.01f; // How much difference is tolerated in travelTime
+
+        // Move player to position where it's close to a patrolling enemy
+        Vector3 newStartPos = new Vector3(19, 1, 1);
+        player.position = newStartPos;
+
+        // Get the needed enemy and patrolPath components
+        GameObject enemy = GetClosestEnemy(player.position);
+        PatrolPath pp = GetClosestPatrolPath(player.position).GetComponent<PatrolPath>();
+        float ppLength = Vector2.Distance(pp.endPosition, pp.startPosition);
+
+        float travelledDistance = 0;
+        Vector2 lastPos = enemy.transform.position;
+
+        // Travel until ppLength has been travelled
+        while(travelledDistance < ppLength)
+        {
+            // Reduce travelTime left and add travelledDistance after every frame
+            yield return null;
+            travelTime -= Time.deltaTime;
+            travelledDistance += Vector2.Distance(lastPos, enemy.transform.position);
+            lastPos = enemy.transform.position;
+
+            // Immediately fail test if too much time was passed
+            if (travelTime < -tolerance)
+            {
+                Debug.Log($"Traveling patrolPath took too long, distance left to travel: {ppLength - travelledDistance}");
+                Assert.Fail();
+            }
+        }
+
+        // Assert that approx travelTime passed to travel ppLength
+        Debug.Log("travelTime left at the end was: " + travelTime);
+        UnityAssert.AreApproximatelyEqual(travelTime, 0, tolerance);
+    }
+
+    // Verifies that player sprite is flipped when it changes facing direction
+    // Should kill mutation 34 and mutation 35
+    [UnityTest]
+    public IEnumerator FlipPlayerSprite()
+    {
+        SpriteRenderer playerSr = player.GetComponent<SpriteRenderer>();
+
+        // Move player left and assert that sprite is flipped
+        input.Press(keyboard.aKey);
+        yield return null;
+        Assert.IsTrue(playerSr.flipX);
+        input.Release(keyboard.aKey);
+
+        // Move player right and assert that sprite is not flipped
+        input.Press(keyboard.dKey);
+        yield return null;
+        Assert.IsTrue(!playerSr.flipX);
+        input.Release(keyboard.dKey);
+    }
+
+    // Verifies that player run animation is shown when player moves on the ground
+    // Should kill mutation 36
+    [UnityTest]
+    public IEnumerator PlayerRunAnimation()
+    {
+        string runAnimationName = "PlayerRun";
+        Animator playerAnim = player.GetComponent<Animator>();
+        AnimatorClipInfo acInfo;
+
+        // Move player left and assert that run animation is playing after a brief wait
+        input.Press(keyboard.aKey);
+        yield return new WaitForSeconds(0.5f);
+        acInfo = playerAnim.GetCurrentAnimatorClipInfo(0)[0];
+        Assert.AreEqual(acInfo.clip.name, runAnimationName);
+        yield return new WaitForSeconds(0.5f);
+        input.Release(keyboard.aKey);
+
+        // Move player right and assert that run animation is playing after a brief wait
+        input.Press(keyboard.dKey);
+        yield return new WaitForSeconds(0.5f);
+        acInfo = playerAnim.GetCurrentAnimatorClipInfo(0)[0];
+        Assert.AreEqual(acInfo.clip.name, runAnimationName);
+        yield return new WaitForSeconds(0.5f);
+        input.Release(keyboard.dKey);
+    }
+
     // ========== HELPER METHODS ========== 
 
     // Disables all enemies from map
@@ -427,4 +707,41 @@ public class PlayModeTests
         UnityAssert.AreApproximatelyEqual(v1.z, v2.z, tolerance);
     }
 
+    // Returns PatrolPath-object that is closest to given position in scene
+    private GameObject GetClosestPatrolPath(Vector3 pos)
+    {
+        GameObject[] patrolPaths = GameObject.FindGameObjectsWithTag("PatrolPath");
+        Transform closestTransform = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject pp in patrolPaths)
+        {
+            float dist = Vector3.Distance(pos, pp.transform.position);
+            if (dist < minDistance)
+            {
+                closestTransform = pp.transform;
+                minDistance = dist;
+            }
+        }
+        return closestTransform.gameObject;
+    }
+
+    // Returns Enemy-object that is closest to given position in scene
+    private GameObject GetClosestEnemy(Vector3 pos)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Transform closestTransform = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject e in enemies)
+        {
+            float dist = Vector3.Distance(pos, e.transform.position);
+            if (dist < minDistance)
+            {
+                closestTransform = e.transform;
+                minDistance = dist;
+            }
+        }
+        return closestTransform.gameObject;
+    }
 }
