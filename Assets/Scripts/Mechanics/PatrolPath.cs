b450d8619cd5eb2c4f030640c0228ca3f4e86df2
Vahv <vahvett@gmail.com>
2022-03-22 14:46:07 +0200

    More refactoring to prevent CREAM parse errors. Does not affect gameplay

--- Assets/Scripts/Mechanics/PatrolPath.cs
+++ Assets/Scripts/Mechanics/PatrolPath.cs
@@ -17,7 +17,22 @@ namespace Platformer.Mechanics
         /// </summary>
         /// <param name="speed"></param>
         /// <returns></returns>
-        public Mover CreateMover(float speed=1) => new Mover(this, speed);
+        public Mover CreateMover(float speed)
+        { 
+            return new Mover(this, speed); 
+        }
+
+        // CUSTOM ADDITION SO OPTIONAL PARAMETER CAN BE REMOVED FROM METHOD ABOVE
+        // AND CREAM PARSE WONT FAIL
+        /// <summary>
+        /// Create a Mover instance which is used to move an entity along the path at a certain speed.
+        /// </summary>
+        /// <param name="speed"></param>
+        /// <returns></returns>
+        public Mover CreateMover()
+        {
+            return new Mover(this, 1);
+        }
 
         void Reset()
         {
