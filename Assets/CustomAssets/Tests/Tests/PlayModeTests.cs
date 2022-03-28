02f5feecc044fb5a6f5c6dc2b63ec208e8f5b796
Vahv <vahvett@gmail.com>
2022-03-28 18:40:24 +0300

    First updates to TestSet2

--- Assets/CustomAssets/Tests/Tests/PlayModeTests.cs
+++ Assets/CustomAssets/Tests/Tests/PlayModeTests.cs
@@ -8,6 +8,7 @@ using UnityEngine.TestTools;
 using UnityEngine.Assertions.Comparers;
 using Platformer.Mechanics;
 using UnityAssert = UnityEngine.Assertions.Assert;
+using UnityEngine.UI;
 
 public class PlayModeTests
 {
@@ -373,7 +374,7 @@ public class PlayModeTests
     [UnityTest]
     public IEnumerator ReachGoal()
     {
-        float moveDuration = 1f; // How long will player press jump-key
+        float moveDuration = 1f; // How long will player move
 
         // Get first goal zone found in the scene and move it to the left of player
         GameObject goalZone = GameObject.Find("Victory");
@@ -406,6 +407,331 @@ public class PlayModeTests
         AssertVectorsApproxEqual(endPos, player.transform.position);
     }
 
+    // ========== Test Set 3 Improvements ========== 
+
+    // Verifies that player has HP again after respawning
+    // Kills at least mutations AOR13, AOR14, AOR15, AOR16..., UOI121, UOI123, 
+    [UnityTest]
+    public IEnumerator SpawnWithHealth()
+    {
+        float moveTime = 3f;
+        float deathAnimationTime = 3f;
+        float spawnAnimationTime = 1.5f;
+
+        // Move forward, enemy should be encountered before moveTime is over
+        input.Press(keyboard.dKey);
+        yield return new WaitForSeconds(moveTime);
+        input.Release(keyboard.dKey);
+
+        // Wait until death animation is done and player is spawned
+        yield return new WaitForSeconds(deathAnimationTime + spawnAnimationTime);
+
+        // Assert that player has HP again
+        Assert.IsTrue(player.GetComponent<Health>().IsAlive);
+    }
+
+    // Verifies that player can collect multiple tokens in quick succession
+    // Kills at least mutations ASR9, ASR10, ASR11
+    [UnityTest]
+    public IEnumerator CollectMultipleTokens()
+    {
+        float jumpDuration = 1.5f; // How long will player press jump-key
+        int tokenAmt = 6;
+        float tokenGap = 0.25f;
+        List<GameObject> tokenObjects = new List<GameObject>();
+
+        // Get multiple tokens from scene and move them in a line above player
+        TokenInstance[] tokenInstances = GameObject.FindObjectsOfType<TokenInstance>();
+        for (int i = 0; i < tokenAmt; i++)
+        {
+            tokenObjects.Add(tokenInstances[i].gameObject);
+            tokenInstances[i].transform.position = origPos + (Vector3.up * 0.5f) + (Vector3.up * tokenGap * i);
+        }
+
+        // Execute Jump
+        input.Press(keyboard.spaceKey);
+        yield return new WaitForSeconds(jumpDuration);
+        input.Release(keyboard.spaceKey);
+
+        // Assert that tokens have been disabled after being collected
+        foreach(GameObject t in tokenObjects)
+        {
+            Assert.IsFalse(t.activeSelf);
+        }
+    }
+
+    // Verifies that idling enemy stays still
+    // Should kill at least mutations LCR1, UOI18, UOI20
+    [UnityTest]
+    public IEnumerator IdleEnemyStaysStill()
+    {
+        float monitorPosDuration = 5f; // How long test should run and keep that enemy isn't moving
+
+        // Get enemy and remove possible patrol path to make it idle
+        GameObject enemy = GetClosestEnemy(player.position);
+        enemy.GetComponent<EnemyController>().path = null;
+        Vector3 enemyOrigPos = enemy.transform.position;
+
+        while(monitorPosDuration > 0)
+        {
+            // Assert that enemy position has stayed the same
+            AssertVectorsApproxEqual(enemy.transform.position, enemyOrigPos);
+
+            // Wait for next frame
+            monitorPosDuration -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    // Verifies that token animation is ran at correct speed
+    // Should kill at least mutations AOR6, ASR1, ASR2, ASR3, ASR4, ROR8, UOI38, UOI41
+    [UnityTest]
+    public IEnumerator TokenAnimationCorrectSpeed()
+    {
+        float tolerance = 0.01f; // How much full animation length is allowed to differ from expected
+        float startLoopTimeout = 2f;
+
+        // Set needed objects
+        float tokenFrameRate = GameObject.FindObjectOfType<TokenController>().frameRate;
+        GameObject token = GameObject.Find("Token");
+        Sprite lastSprite = token.GetComponent<SpriteRenderer>().sprite;
+        Sprite[] tokenSprites = token.GetComponent<TokenInstance>().idleAnimation;
+
+        // List of sprites that have already been displayed
+        List<Sprite> alreadyDisplayedSprites = new List<Sprite>();
+
+        // Time it should take to go through all animation frames. Note -1 because dont need to wait for first frame.
+        float rotationTime = (1 / tokenFrameRate) * (tokenSprites.Length - 1);
+
+        // Wait at start until new animation frame has just been changed
+        while(lastSprite == token.GetComponent<SpriteRenderer>().sprite && startLoopTimeout > 0)
+        {
+            startLoopTimeout -= Time.deltaTime;
+            yield return null;
+        }
+
+        // Do until all animation frames have been displayed
+        while(alreadyDisplayedSprites.Count < tokenSprites.Length)
+        {
+            // Wait until next frame and set variables again
+            yield return null;
+            rotationTime -= Time.deltaTime;
+            Sprite currentSprite = token.GetComponent<SpriteRenderer>().sprite;
+
+            // If sprite was changed from last, add new sprite to Displayed-list
+            if (currentSprite != lastSprite)
+            {
+                alreadyDisplayedSprites.Add(currentSprite);
+                lastSprite = currentSprite;
+            }
+
+            // If too long has passed and all sprites are still not displayed, FAIL test
+            if (rotationTime < -tolerance)
+            {
+                Debug.Log($"Playing full token animation took too long, current amount of frames displayed:" +
+                    $" {alreadyDisplayedSprites.Count}/{tokenSprites.Length}");
+                Assert.Fail();
+            }
+        }
+
+        // Assert that approx animation rotation time passed to display all frames
+        Debug.Log("rotationTime left at the end was: " + rotationTime);
+        UnityAssert.AreApproximatelyEqual(rotationTime, 0, tolerance);
+    }
+
+    // Verifies that player health is 0 after death
+    // Kills at least mutations ROR31, ROR34, UOI134
+    [UnityTest]
+    public IEnumerator PlayerHealthZeroAfterDeath()
+    {
+        float moveTime = 3f;
+        float spawnTime = 4f;
+
+        // Move forward, enemy should be encountered before moveTime is over
+        input.Press(keyboard.dKey);
+        yield return new WaitForSeconds(moveTime);
+        input.Release(keyboard.dKey);
+
+        // Assert that player health is 0
+        Assert.IsFalse(player.GetComponent<Health>().IsAlive);
+
+        // Wait until player spawn to avoid weird FAILs in next tests
+        yield return new WaitForSeconds(spawnTime);
+    }
+
+    // Verifies that different tabs can be opened in Pause-menu
+    // Kills at least mutations UOI4, UOI7, UOI10, UOI11...
+    [UnityTest]
+    public IEnumerator OpenMenuTabs()
+    {
+        // Open menu and get UI-buttons
+        input.PressAndRelease(keyboard.escapeKey);
+        Button settingsButton = GameObject.Find("SettingsButton").GetComponent<Button>();
+        Button controlsButton = GameObject.Find("ControlsButton").GetComponent<Button>();
+        Button creditsButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+
+        // Click through all buttons and check that correct tab is open after it
+        // Clicks should be simulated through Mouse-input but that doesn't seem to work
+        settingsButton.onClick.Invoke();
+        GameObject settingsScreen = GameObject.Find("Settings");
+        Assert.IsTrue(settingsScreen.activeInHierarchy);
+
+        controlsButton.onClick.Invoke();
+        GameObject controlsScreen = GameObject.Find("Controls");
+        Assert.IsTrue(controlsScreen.activeInHierarchy);
+
+        creditsButton.onClick.Invoke();
+        GameObject creditsScreen = GameObject.Find("Credits");
+        Assert.IsTrue(creditsScreen.activeInHierarchy);
+
+        yield return null;
+    }
+
+    // Verifies that enemy sprite is flipped when it changes facing direction
+    // Kills at least mutations UOI22
+    [UnityTest]
+    public IEnumerator FlipEnemySprite()
+    {
+        float timeout = 10f;
+        // Test not started until enemy is this far away from PatrolPath start/end
+        float startPointThreshold = 0.5f;
+        // Move player to position where it's close to an enemy
+        Vector3 newStartPos = new Vector3(19, 1, 1);
+        player.position = newStartPos;
+
+        // Get the needed enemy components
+        GameObject enemy = GetClosestEnemy(player.position);
+        SpriteRenderer enemySr = enemy.GetComponent<SpriteRenderer>();
+        GameObject pp = GetClosestPatrolPath(player.position);
+
+        // Get PatrolPath start and end positions as scene coordinates
+        float ppStartX = pp.GetComponent<PatrolPath>().startPosition.x + pp.transform.position.x;
+        float ppEndX = pp.GetComponent<PatrolPath>().endPosition.x + pp.transform.position.x;
+
+        // Wait until enemy is not close to end points of patrol path to avoid weird test fails
+        while (enemy.transform.position.x - ppEndX > -startPointThreshold || enemy.transform.position.x - ppStartX < startPointThreshold)
+        {
+            yield return null;
+            timeout -= Time.deltaTime;
+            if (timeout < 0)
+            {
+                Debug.Log("Timeout while waiting for enemy to be further away from patrol path end points");
+                Assert.Fail();
+                break;
+            }
+        }
+
+        // Save enemy startPosX
+        float startPosX = enemy.transform.position.x;
+
+        // Wait until moving left
+        yield return new WaitUntil(() => enemy.transform.position.x < startPosX);
+        Debug.Log("Enemy moving left");
+        // Assert that sprite is flipped
+        Assert.IsTrue(enemySr.flipX);
+
+        // Wait until moving right
+        yield return new WaitUntil(() => enemy.transform.position.x > startPosX);
+        Debug.Log("Enemy moving right");
+        // Assert that sprite is not flipped
+        Assert.IsTrue(!enemySr.flipX);
+    }
+
+    // Verifies that patrolling enemymoves to both end positions of its patrol path
+    // Kills at least mutations UOI32
+    [UnityTest]
+    public IEnumerator EnemyVisitsPatrolPathEnds()
+    {
+        // How much enemy's end point can differentiate from patrol path end points
+        float endPointTolerance = 0.35f;
+        float monitorMovementDuration = 5f;
+
+        // Move player to position where it's close to a patrolling enemy
+        Vector3 newStartPos = new Vector3(19, 1, 1);
+        player.position = newStartPos;
+
+        // Get the needed enemy and patrolPath components
+        GameObject enemy = GetClosestEnemy(player.position);
+        PatrolPath pp = GetClosestPatrolPath(player.position).GetComponent<PatrolPath>();
+        float ppStartX = pp.transform.position.x + pp.startPosition.x;
+        float ppEndX = pp.transform.position.x + pp.endPosition.x;
+
+        // Variables for tracking enemy's max and min position
+        float minX = ppEndX;
+        float maxX = ppStartX;
+
+        // Monitor movement and get max and min points of enemy x-position
+        while(monitorMovementDuration > 0)
+        {
+            // Reduce monitor duration each frame
+            yield return null;
+            monitorMovementDuration -= Time.deltaTime;
+
+            if (enemy.transform.position.x < minX) minX = enemy.transform.position.x;
+            if (enemy.transform.position.x > maxX) maxX = enemy.transform.position.x;
+        }
+
+        // Assert that max and min points are approx equal to patrol path end points
+        UnityAssert.AreApproximatelyEqual(minX, ppStartX, endPointTolerance);
+        UnityAssert.AreApproximatelyEqual(maxX, ppEndX, endPointTolerance);
+    }
+
+    // Verifies that token collect animation is played when player hits token
+    // Should kill at least mutations UOI62
+    [UnityTest]
+    public IEnumerator TokenCollectAnimation()
+    {
+        float timeoutDuration = 2f; // How long to wait until test fails
+
+        // Get first token found in the scene and move it above player
+        GameObject token = GameObject.Find("Token");
+        token.transform.position = origPos + Vector3.up;
+        // Set needed objects
+        Sprite lastSprite = token.GetComponent<SpriteRenderer>().sprite;
+        List<Sprite> collectedSprites = new List<Sprite>(token.GetComponent<TokenInstance>().collectedAnimation);
+        // List of sprites that have already been displayed
+        List<Sprite> alreadyDisplayedSprites = new List<Sprite>();
+
+        // Execute Jump to collect token
+        input.Press(keyboard.spaceKey);
+
+        // Do until all animation frames have been displayed
+        while (alreadyDisplayedSprites.Count < collectedSprites.Count)
+        {
+            // Wait until next frame and set variables again
+            yield return null;
+            timeoutDuration -= Time.deltaTime;
+            Sprite currentSprite = token.GetComponent<SpriteRenderer>().sprite;
+
+            // If sprite was changed from last and is a collected-sprite, add it to displayed sprites list
+            if (currentSprite != lastSprite && collectedSprites.Contains(currentSprite))
+            {
+                alreadyDisplayedSprites.Add(currentSprite);
+                lastSprite = currentSprite;
+            }
+
+            // If too long has passed and all sprites are still not displayed, FAIL test
+            if (timeoutDuration < 0)
+            {
+                Debug.Log($"Took too long to display all Collected_Token sprites, timing out");
+                Debug.Log($"Displayed Collected_Token sprites: {alreadyDisplayedSprites.Count}/{collectedSprites.Count}");
+                Assert.Fail();
+            }
+        }
+
+        input.Release(keyboard.spaceKey);
+    }
+
+    // Verifies that jump sound effect is played when player jumps
+    // Should kill at least mutations UOI135, UOI136, UOI137
+    [UnityTest]
+    public IEnumerator PlayerJumpAudio()
+    {
+        yield return null;
+    }
+
+
+
     // ========== HELPER METHODS ========== 
 
     // Disables all enemies from map
@@ -425,6 +751,43 @@ public class PlayModeTests
         UnityAssert.AreApproximatelyEqual(v1.x, v2.x, tolerance);
         UnityAssert.AreApproximatelyEqual(v1.y, v2.y, tolerance);
         UnityAssert.AreApproximatelyEqual(v1.z, v2.z, tolerance);
+    }
+
+    private GameObject GetClosestEnemy(Vector3 pos)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Transform closestTransform = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject e in enemies)
+        {
+            float dist = Vector3.Distance(pos, e.transform.position);
+            if (dist < minDistance)
+            {
+                closestTransform = e.transform;
+                minDistance = dist;
+            }
+        }
+        return closestTransform.gameObject;
+    }
+
+    // Returns PatrolPath-object that is closest to given position in scene
+    private GameObject GetClosestPatrolPath(Vector3 pos)
+    {
+        GameObject[] patrolPaths = GameObject.FindGameObjectsWithTag("PatrolPath");
+        Transform closestTransform = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject pp in patrolPaths)
+        {
+            float dist = Vector3.Distance(pos, pp.transform.position);
+            if (dist < minDistance)
+            {
+                closestTransform = pp.transform;
+                minDistance = dist;
+            }
+        }
+        return closestTransform.gameObject;
     }
 
 }
